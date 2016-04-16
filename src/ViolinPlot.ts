6c36cd9b79d764940c52482cd2f7a83a1f0e98c5
Panos <codiply@gmail.com>
2016-04-16 21:08:40 +0100

    Add typescript file and compile it to js file

---
+++ src/ViolinPlot.ts
@@ -0,0 +1,13 @@
+/// <reference path="../typings/d3/d3.d.ts"/>;
+
+module semio {
+    export class violinplot {
+        
+        constructor() {
+        }
+ 
+        public draw() {
+            alert('This is a violin plot!');
+        }
+    }
+}

32c0540b412e6bddde3c4e252bb8f9c06bed7335
Panos <codiply@gmail.com>
2016-04-18 20:07:54 +0100

    Add axis and plot data points correctly

--- src/ViolinPlot.ts
+++ src/ViolinPlot.ts
@@ -38,29 +38,39 @@ module semio {
             return this;
         } 
         
-        numeric(accessor: (d: any) => number): violinplot {
-            this._numericAccessor = accessor;
+        numeric(accessor: (d: any) => number | string): violinplot {
+            this._numericAccessor = function (d) {
+                return +accessor(d);
+            };;
             return this;
         } 
         
         draw(containerSelector: string): void {
             if (!this.data)
                 return;
-                
-            let xExtent = d3.extent(this.data, this._numericAccessor);
-            let xScale = d3.scale.linear()
-                .domain(xExtent)
-                .range([0.1 * this._width, 0.9 * this._width]);
-            
-            let categoricalValues = d3.set(this.data.map(this._categoricalAccessor)).values();
-            let yScale = d3.scale.ordinal()
-                .domain(categoricalValues)
-                .rangePoints([0.1 * this._height, 0.9 * this._height]);
-            
-            var svg = d3.select(containerSelector)
+           
+            let svg = d3.select(containerSelector)
                 .append('svg')
                 .attr('width', this._width)
                 .attr('height', this._height);
+
+            let categoricalValues = d3.set(this.data.map(this._categoricalAccessor)).values();
+            let xScale = d3.scale.ordinal()
+                .domain(categoricalValues)
+                .rangePoints([0.1 * this._width, 0.9 * this._width]);
+            let xAxis = d3.svg.axis().scale(xScale).orient('bottom');
+            let xAxisGroup = svg.append('g')
+                .attr('transform', 'translate(0,' + (0.92 * this._height) + ')')
+                .call(xAxis);
+            
+            let yExtent = d3.extent(this.data, this._numericAccessor);
+            let yScale = d3.scale.linear()
+                .domain(yExtent)
+                .range([0.9 * this._height, 0.1 * this._height]);
+            let yAxis = d3.svg.axis().scale(yScale).orient('left');
+            let yAxisGroup = svg.append('g')
+                .attr('transform', 'translate(' + (0.1 * this._height) + ',0)')
+                .call(yAxis);
             
             svg.selectAll('circle')
                 .data(this.data)
@@ -68,8 +78,8 @@ module semio {
                 .append('circle')
                 .attr('fill', 'black')
                 .attr('r', 5)
-                .attr('cx', d => xScale(this._numericAccessor(d)))
-                .attr('cy', d => yScale(this._categoricalAccessor(d)));
+                .attr('cx', d => xScale(this._categoricalAccessor(d)))
+                .attr('cy', d => yScale(this._numericAccessor(d)));
         }
     }
 }

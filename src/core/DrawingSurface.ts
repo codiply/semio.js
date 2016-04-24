09cdf66bcc6db28b0edd0f0a8052897d607a8c5a
Panos <codiply@gmail.com>
2016-04-24 17:22:50 +0100

    Implement methods for splitting a drawing surface

--- src/core/DrawingSurface.ts
+++ src/core/DrawingSurface.ts
@@ -7,7 +7,8 @@ module semio.core {
     export class DrawingSurface implements Surface {
         private width: number;
         private height: number;
-        private isSplit: number;
+        private x: number;
+        private y: number;
         
         svg: d3.Selection<any>;
         
@@ -28,18 +29,60 @@ module semio.core {
             return this;
         }
         
+        setX(x: number): Surface {
+            this.x = x;
+            this.svg.attr('x', x);
+            return this;
+        }
+        
+        setY(y: number): Surface {
+            this.y = y;
+            this.svg.attr('y', y);
+            return this;
+        }
+        
         getWidth(): number { return this.width; }
         getHeight(): number { return this.height; }
         
         splitRows(n: number): Array<Surface> {
-
-            return [];
+            let rowHeight = this.height / n;
+            return _.range(0, n).map((row) => {
+                return new DrawingSurface(this.containerId + '_row_' + row.toString())
+                    .setHeight(rowHeight)
+                    .setWidth(this.width)
+                    .setY(row * rowHeight); 
+            });
         }
+        
         splitColumns(n: number): Array<Surface> {
-            return [];
+            let columnWidth = this.width / n;
+            return _.range(0, n).map((col) => {
+                return new DrawingSurface(this.containerId + '_column_' + col.toString())
+                    .setHeight(this.height)
+                    .setWidth(columnWidth)
+                    .setX(col * columnWidth);
+            });
         }
-        splitGrid(n: number): Array<Surface> {
-            return [];
+        
+        splitGrid(n: number, maxColumns: number): Array<Array<Surface>> {
+            let nColumns = Math.min(n, maxColumns);
+            let nRows = Math.ceil(n / nColumns);
+            
+            let cellWidth = this.width / nColumns;
+            let cellHeight = this.height / nRows;
+            
+            return _.range(0, nRows).map((row) => {
+                return _.range(0, nColumns).filter((col) => {
+                    let i = row * nColumns + col;
+                    return i < n; 
+                }).map((col) => {
+                    return new DrawingSurface(this.containerId + '_row_' + row.toString() + '_column_' + col.toString())
+                        .setHeight(cellHeight)
+                        .setWidth(cellHeight)
+                        .setX(col * cellHeight)
+                        .setY(row * cellHeight);
+                });
+            })
         }
     }
 }
--- src/interfaces/Surface.ts
+++ src/interfaces/Surface.ts
@@ -1,14 +1,20 @@
 /// <reference path="../../typings/d3/d3.d.ts"/>
+/// <reference path="../../typings/lodash/lodash.d.ts"/>
 
 module semio.interfaces {
     export interface Surface {
         svg: d3.Selection<any>;
+        
         setWidth(width: number): Surface;
         setHeight(width: number): Surface;
+        setX(x: number): Surface;
+        setY(y: number): Surface;
+        
         getWidth(): number;
         getHeight(): number;
+        
         splitRows(n: number): Array<Surface>;
         splitColumns(n: number): Array<Surface>;
-        splitGrid(n: number): Array<Surface>;
+        splitGrid(n: number, maxColumns: number): Array<Array<Surface>>;
     }
 }

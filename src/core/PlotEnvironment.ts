60b0d01a76a2d683685e1e93b69d7c1481e235b6
Panos <codiply@gmail.com>
2016-04-26 22:00:30 +0100

    Make PlotEnvironment behave like immutable

--- src/core/PlotEnvironment.ts
+++ src/core/PlotEnvironment.ts
@@ -1,3 +1,4 @@
+/// <reference path="../../typings/lodash/lodash.d.ts"/>
 /// <reference path="../interfaces/Environment.ts"/>
 
 module semio.core {
@@ -5,25 +6,34 @@ module semio.core {
     import Environment = semio.interfaces.Environment;
     
     export class PlotEnvironment implements Environment {
-        private categoryColours: { [column: string]: (value: string) => string } = { };
-        private categoryValues: { [column: string]: Array<string> } = { };
+        private _categoryColours: { [column: string]: (value: string) => string } = { };
+        private _categoryValues: { [column: string]: Array<string> } = { };
 
         setCategoryValues(column: string, values: Array<string>): Environment {
-            this.categoryValues[column] = values;
-            return this;
+            let clone = this.clone();
+            clone._categoryValues[column] = values.slice(0);
+            return clone;
         }
         
         setCategoryColours(column: string, colours: (value: string) => string): Environment {
-            this.categoryColours[column] = colours;
-            return this;
+            let clone = this.clone();
+            clone._categoryColours[column] = colours;
+            return clone;
         }
         
-        getCategoryValues(): { [column: string]: Array<string>} {
-            return this.categoryValues;
+        getCategoryValues(): { [column: string]: Array<string>} {            
+            return this._categoryValues;
         }
         
         getCategoryColours(): { [column: string]: (value: string) => string } {
-            return this.categoryColours;
+            return this._categoryColours;
+        }
+        
+        private clone(): PlotEnvironment {
+            let clone = new PlotEnvironment();
+            clone._categoryColours = _.clone(this._categoryColours);
+            clone._categoryValues = _.clone(this._categoryValues);
+            return this;
         }
     }
 }

ea87a5a90b1bfd71c723aed61abf921df39f45b9
Panos <codiply@gmail.com>
2016-04-27 19:03:33 +0100

    Add CategoricalPlot class placeholder

---
+++ src/chart/CategoricalPlot.ts
@@ -0,0 +1,45 @@
+/// <reference path="../../typings/d3/d3.d.ts"/>
+/// <reference path="../../typings/lodash/lodash.d.ts"/>
+/// <reference path="../interfaces/Environment.ts"/>
+/// <reference path="../interfaces/Plotable.ts"/>
+/// <reference path="../interfaces/Surface.ts"/>
+
+module semio.chart {
+    import Environment = semio.interfaces.Environment;
+    import Plotable = semio.interfaces.Plotable;
+    import Surface = semio.interfaces.Surface;
+    import VerticalViolin = semio.shape.VerticalViolin;
+    
+    export class CategoricalPlot implements Plotable {
+        private _xMargin: number = 0.1;
+        private _yMargin: number = 0.1;
+        private _splitOnColumn: string;
+        private _categoricalAccessor: (d: any) => string;
+        private _numericAccessor: (d: any) => number;
+        private _categoryColumns: Array<string> = [];
+
+        value(column: string): CategoricalPlot {
+            this._numericAccessor = function (d) {
+                return +d[column];
+            };
+            return this;
+        } 
+        
+        splitOn(column: string): CategoricalPlot {
+            this._categoricalAccessor = function (d) {
+                return d[column].toString();
+            };
+            this._splitOnColumn = column;
+            this._categoryColumns.push(column);
+            return this;
+        } 
+        
+        getCategoryColumns(): Array<string> {
+            return this._categoryColumns;
+        }
+        
+        plot(data: Array<any>, plotables: Array<Plotable>, surface: Surface, environment: Environment): void {
+            
+        }
+    }
+}

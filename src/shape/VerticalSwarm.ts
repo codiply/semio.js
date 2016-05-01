ca1a1d82ba6179b4781bf28c008d905ef0e53385
Panos <codiply@gmail.com>
2016-05-01 22:38:56 +0100

    First version of swarm plot

--- src/shape/VerticalSwarm.ts
+++ src/shape/VerticalSwarm.ts
@@ -6,10 +6,26 @@ module semio.shape {
     import Context = semio.interfaces.Context;
     import Surface = semio.interfaces.Surface;
     
-    interface SwarmPoint {
-        x: number,
-        y: number,
-        color: string
+    class SwarmPoint {
+        
+        constructor(public x: number, 
+                    public y: number, 
+                    public color: string) { }
+        
+        setX(newX: number): SwarmPoint {
+            return new SwarmPoint(newX, this.y, this.color);    
+        }
+        
+        distance(p: SwarmPoint): number {
+            let dx = p.x - this.x;
+            let dy = p.y - this.y;
+            return Math.sqrt(dx * dx + dy * dy);
+        }
+        placeNextTo(p: SwarmPoint, diameter: number): Array<SwarmPoint> { 
+            let dy = p.y - this.y;
+            let dx = Math.sqrt(diameter * diameter - dy * dy) * 1.05;
+            return [this.setX(p.x - dx), this.setX(p.x + dx)];
+        }
     }
     
     export class VerticalSwarm {        
@@ -37,22 +53,28 @@ module semio.shape {
         }
         
         draw(data: Array<any>, surface: Surface, context: Context): void {
-            let cx = surface.getWidth() / 2;
+            let centre = surface.getWidth() / 2;
 
             var yScale = context.getYScale(this._valueColumn);            
             let colors = context.getCategoryColours()[this._colorColumn];
             
-            let swarmPoints: Array<SwarmPoint> = _.chain(data).map((d) => {
-                return {
-                    x: cx,
-                    y: yScale(this._numericAccessor(d)),
-                    color: colors(d[this._colorColumn])
-                }
-            }).orderBy((p) => p.y).value();
+            let startingPositions: Array<SwarmPoint> = _.chain(data).map((d) => {
+                return new SwarmPoint(centre, 
+                                      yScale(this._numericAccessor(d)), 
+                                      colors(d[this._colorColumn]));
+            }).orderBy((p) => -p.y).value();
+            
+            let swarm: Array<SwarmPoint> = [];
+            startingPositions.forEach((p) => {
+                let neighbors = _.chain(swarm).filter((p2) => Math.abs(p.y - p2.y) < this._diameter)
+                    .sortBy((p) => Math.abs(p.x - centre))
+                    .value();
+                swarm.push(this.findPosition(p, neighbors, centre));
+            });
             
             surface.svg.append('g')
                 .selectAll('circle')
-                .data(swarmPoints)
+                .data(swarm)
                 .enter()
                 .append('circle')
                 .attr('cx', d => d.x)
@@ -60,5 +82,16 @@ module semio.shape {
                 .attr('r', this._diameter / 2)
                 .style('fill', d => d.color);
         }
+        
+        findPosition(p: SwarmPoint, neighbors: Array<SwarmPoint>, centre: number): SwarmPoint {
+            let candidatePositions = _.flatMap(neighbors, (n) => p.placeNextTo(n, this._diameter));
+            candidatePositions.push(p);
+            let goodCandidates = _.chain(candidatePositions)
+                .filter((c) => _.findIndex(neighbors, (n) => c.distance(n) < this._diameter) === -1)
+                .sortBy((c) => Math.abs(c.x - centre))
+                .value();
+            return goodCandidates[0];
+        }
+        
     }
 }

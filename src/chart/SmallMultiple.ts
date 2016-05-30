22f720d3221ee01ec50b5013b6e1927c99e4c580
Panos <codiply@gmail.com>
2016-05-30 21:26:17 +0100

    Linting fix for SmallMultiple

--- src/chart/SmallMultiple.ts
+++ src/chart/SmallMultiple.ts
@@ -23,75 +23,76 @@ namespace semio.chart {
         private _splitOnColumn: string;
         private _categoricalAccessor: (d: any) => string;
         private _plotable: Plotable;
-        
+
         constructor() { }
-        
-        maxColumns(maxColumns: number): SmallMultiple {
+
+        public maxColumns(maxColumns: number): SmallMultiple {
             this._maxColumns = maxColumns;
             return this;
         }
-        
-        splitOn(column: string): SmallMultiple {
+
+        public splitOn(column: string): SmallMultiple {
             this._categoricalAccessor = function (d) {
                 return d[column].toString();
             };
             this._splitOnColumn = column;
             return this;
         }
-        
-        headerRatio(ratio: number): SmallMultiple {
+
+        public headerRatio(ratio: number): SmallMultiple {
             this._headerRatio = ratio;
             return this;
         }
-        
-        horizontalSpacingRatio(ratio: number): SmallMultiple {
+
+        public horizontalSpacingRatio(ratio: number): SmallMultiple {
             this._horizontalSpacingRatio = ratio;
             return this;
         }
-        
-        verticalSpacingRatio(ratio: number): SmallMultiple {
+
+        public verticalSpacingRatio(ratio: number): SmallMultiple {
             this._verticalSpacingRatio = ratio;
             return this;
         }
-        
-        getCategoricalColumns(): Array<string> {
+
+        public getCategoricalColumns(): Array<string> {
             let columns = this._plotable.getCategoricalColumns().slice(0);
             columns.push(this._splitOnColumn);
             return columns;
         }
-        
-        getNumericColumns(): Array<string> {
+
+        public getNumericColumns(): Array<string> {
             return this._plotable.getNumericColumns().slice(0);
         }
-        
-        add(plotable: Plotable): Plotable {
+
+        public add(plotable: Plotable): Plotable {
             this._plotable = plotable;
             return this;
         }
-        
-        plot(data: Array<any>, surface: Surface, context: Context): void {
-            if (!data || !this._plotable)
+
+        public plot(data: Array<any>, surface: Surface, context: Context): void {
+            if (!data || !this._plotable) {
                 return;
-            
+            }
+
             let groupedData = d3.nest().key(this._categoricalAccessor).entries(data);
             let categories = groupedData.map((g) => g.key);
-        
+
             let subSurfaces = surface.splitGrid(categories.length, 
-                this._maxColumns, this._horizontalSpacingRatio, this._verticalSpacingRatio); 
-            
+                this._maxColumns, this._horizontalSpacingRatio, this._verticalSpacingRatio);
+
             let updatedContext = this.fixNumericRanges(data, context);
-            
+
             categories.forEach((cat, i) => {
                 let splitSurface = subSurfaces[i].splitHeader(this._headerRatio);
-                
+
                 let title = this._splitOnColumn + ': ' + cat;
                 Text.placeTitle(splitSurface.header, title);
-                
+
                 let updatedContextWithSlice = updatedContext.setSlicedColumnValue(this._splitOnColumn, cat);
                 this._plotable.plot(groupedData[i].values, splitSurface.body, updatedContextWithSlice);
             });
         }
-        
+
         private fixNumericRanges(data: Array<any>, context: Context): Context {
             let newContext = context;
             this._plotable.getNumericColumns().forEach((col) => {

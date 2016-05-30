e07c420be33fb55ec7decd3abf9a3f125d0de812
Panos <codiply@gmail.com>
2016-05-30 21:10:23 +0100

    Linting fix for Visualization

--- src/Visualization.ts
+++ src/Visualization.ts
@@ -15,49 +15,50 @@ namespace semio {
     import Context = semio.interfaces.Context;
     import Plotable = semio.interfaces.Plotable;
 
-    export class Visualization {        
+    export class Visualization {
         private _width: number = 800;
         private _height: number = 800;
         private _plotable: Plotable;
-        
+
         constructor(private containerId: string) { }
-        
-        width(width: number): Visualization {
+
+        public width(width: number): Visualization {
             this._width = width;
             return this;
         }
-        
-        height(height: number): Visualization {
+
+        public height(height: number): Visualization {
             this._height = height;
             return this;
         }
-        
-        add(plotable: Plotable): Visualization {
+
+        public add(plotable: Plotable): Visualization {
             this._plotable = plotable;
             return this;
         }
-        
-        plot(data: Array<any>): void {
-            if (!data || !this._plotable)
+
+        public plot(data: Array<any>): void {
+            if (!data || !this._plotable) {
                 return;
-            
-            var surface = new DrawingSurface(this.containerId)
+            }
+
+            let surface = new DrawingSurface(this.containerId)
                 .setWidth(this._width)
                 .setHeight(this._height);
-           
+
             let context: Context = new PlotContext();
-            
+
             let tooltip = new PlotTooltip(this.containerId);
             context = context.setTooltip(tooltip);
-            
+
             this._plotable.getCategoricalColumns().forEach((column) => {
-                let values = d3.set(data.map(function(d) { return d[column]; })).values();
-                context = context.setCategoryValues(column, values); 
-                
-                var colors = ColorPalette.qualitative(values);
+                let values = d3.set(data.map((d) => d[column])).values();
+                context = context.setCategoryValues(column, values);
+
+                let colors = ColorPalette.qualitative(values);
                 context = context.setCategoryColours(column, colors);
-            });            
-            
+            });
+
             this._plotable.plot(data, surface, context);
         }
     }

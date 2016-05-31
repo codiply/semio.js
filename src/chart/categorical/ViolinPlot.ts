c46efd2e1995a520ac5d8e6f312e6feb4d0bf565
Panos <codiply@gmail.com>
2016-05-31 20:08:35 +0100

    Linting fix for ViolinPlot

--- src/chart/categorical/ViolinPlot.ts
+++ src/chart/categorical/ViolinPlot.ts
@@ -11,44 +11,44 @@ module semio.chart.categorical {
     import Surface = semio.interfaces.Surface;
     import VerticalViolin = semio.shape.VerticalViolin;
     import PreDrawResult = semio.shape.VerticalViolinPreDrawResult;
-    
+ 
     const SCALE_METHOD_WIDTH = 'width';
     const SCALE_METHOD_COUNT = 'count';
     const SCALE_METHOD_AREA = 'area';
-    
+
     export class ViolinPlot implements CategoricalPlotable {
-        
+
         private _valueColumn: string;
         private _splitOnColumn: string;
-        
+
         private _numericAccessor: (d: any) => number;
         private _categoricalAccessor: (d: any) => string;
-        
+
         private _scaleMethod: string = SCALE_METHOD_WIDTH;
         private _extend: number = 1;
         private _delay: number = 2000;
-        
-        value(column: string): ViolinPlot {
+
+        public value(column: string): ViolinPlot {
             this._valueColumn = column;
             this._numericAccessor = function (d) {
                 return +d[column];
             };
             return this;
-        } 
-        
-        splitOn(column: string): ViolinPlot {
+        }
+
+        public splitOn(column: string): ViolinPlot {
             this._splitOnColumn = column;
             this._categoricalAccessor = function (d) {
                 return d[column].toString();
             };
             return this;
-        } 
-        
-        getLegendColumn(): string {
+        }
+
+        public getLegendColumn(): string {
             return this._splitOnColumn;
         }
-        
-        scale(method: string): ViolinPlot {
+
+        public scale(method: string): ViolinPlot {
             let methodLower = method.toLowerCase();
             if (methodLower === SCALE_METHOD_WIDTH) {
                 this._scaleMethod = SCALE_METHOD_WIDTH;
@@ -57,40 +57,41 @@ module semio.chart.categorical {
             }
             return this;
         }
-        
-        extend(extend: number): ViolinPlot {
+
+        public extend(extend: number): ViolinPlot {
             this._extend = extend;
             return this;
         }
-        
-        delay(delay: number): ViolinPlot {
+
+        public delay(delay: number): ViolinPlot {
             this._delay = delay;
             return this;
         }
-        
-        getCategoricalColumns(): Array<string> {
+
+        public getCategoricalColumns(): Array<string> {
             return [];
         }
-        
-        getNumericColumns(): Array<string> {
+
+        public getNumericColumns(): Array<string> {
             if (this._valueColumn) {
                 return [this._valueColumn];
             }
             return [];
         }
-        
-        plot(data: Array<any>, surface: Surface, context: Context): void {
-            if (!data)
+
+        public plot(data: Array<any>, surface: Surface, context: Context): void {
+            if (!data) {
                 return;
-            
+            }
+
             var yScale = context.getYScale(this._valueColumn);
-            
+
             if (this._splitOnColumn) {
                 var xScale = context.getXScale(this._splitOnColumn);
                 var categories = context.getCategoryValues(this._splitOnColumn);
-                
+
                 var categoryWidth = surface.getWidth() / categories.length;
-                
+
                 let groupedData = d3.nest().key(this._categoricalAccessor).entries(data);
 
                 let preDrawResults: { [category: string]: PreDrawResult }= { };
@@ -99,16 +100,16 @@ module semio.chart.categorical {
                     if (group.values) {
                         var category = group.key;
                         var categoryColor = context.getCategoryColours(this._splitOnColumn)(category);                 
-                        
+
                         let violin = this.constructVerticalViolin().fill(categoryColor);
                         let preDrawResult = violin.preDraw(group.values);
                         preDrawResults[category] = preDrawResult;
                         violins[category] = violin;
                     }
                 });
-                
+
                 let scaleForCategory = (cagetory: string) => 1;
-                
+
                 if (this._scaleMethod === SCALE_METHOD_COUNT) {
                     let counts = _.values(preDrawResults).map((d: PreDrawResult) => d.count)
                     let maxCount = d3.max(counts);
@@ -122,12 +123,12 @@ module semio.chart.categorical {
                         return preDrawResults[category].maxDensity / maxMaxDensity;  
                     };
                 }
-                
+
                 _.forOwn(groupedData, (group) => {
                     var category = group.key;
                     let subSurface = surface
                         .addCenteredColumn('_violin_' + category, xScale(category), categoryWidth);
-                                
+
                     let updatedContext = context.setSlicedColumnValue(this._splitOnColumn, category);
                     violins[category].draw(subSurface, updatedContext, scaleForCategory(category));
                 });    
@@ -137,7 +138,7 @@ module semio.chart.categorical {
                 violin.draw(surface, context, 1);
             }
         }
-        
+
         private constructVerticalViolin(): VerticalViolin {
             return new VerticalViolin()
                 .value(this._valueColumn)

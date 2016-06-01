c25a7ec37fdcc8525ffed36c1369186332359a47
Panos <codiply@gmail.com>
2016-06-01 22:14:42 +0100

    Linting fixes

--- src/chart/HeatMap.ts
+++ src/chart/HeatMap.ts
@@ -14,38 +14,39 @@ module semio.chart {
         private _yColumn: string;
         private _radiusColumn: string;
         private _colorColumn: string;
-        
-        xColumn(column: string): HeatMap {
+
+        public xColumn(column: string): HeatMap {
             this._xColumn = column;
             return this;
         }
-        
-        yColumn(column: string): HeatMap {
+
+        public yColumn(column: string): HeatMap {
             this._yColumn = column;
             return this;
         }
-        
-        radiusColumn(column: string): HeatMap { 
+
+        public radiusColumn(column: string): HeatMap { 
             this._radiusColumn = column;
             return this;
         }
-        
-        colorColumn(column: string): HeatMap {
+
+        public colorColumn(column: string): HeatMap {
             this._colorColumn = column;
             return this;
         }
-        
-        getCategoricalColumns(): Array<string> {
+
+        public getCategoricalColumns(): Array<string> {
             return _.filter([this._xColumn, this._yColumn], _.negate(_.isNull));
         }
-        
-        getNumericColumns(): Array<string> {
+
+        public getNumericColumns(): Array<string> {
             return _.filter([this._radiusColumn, this._colorColumn], _.negate(_.isNull));
         }
-        
-        plot(data: Array<any>, surface: Surface, context: Context): void {
-            if (!data)
+
+        public plot(data: Array<any>, surface: Surface, context: Context): void {
+            if (!data) {
                 return;
+            }
         }
     }
 }
--- src/core/DrawingSurface.ts
+++ src/core/DrawingSurface.ts
@@ -4,48 +4,48 @@
 module semio.core {
     import Surface = semio.interfaces.Surface;
     import SurfaceHeaderBody = semio.interfaces.SurfaceHeaderBody;
-    
+
     export class DrawingSurface implements Surface {
+        public svg: d3.Selection<any>;
+
         private _width: number;
         private _height: number;
         private _x: number;
         private _y: number;
-        
-        svg: d3.Selection<any>;
-        
+
         constructor(public containerId: string) { 
             this.svg = d3.select("#" + containerId)
                          .append("svg");
         }
-        
-        setWidth(width: number): Surface {
+
+        public setWidth(width: number): Surface {
             this._width = width;
             this.svg.attr("width", width);
             return this;
         }
-        
-        setHeight(height: number): Surface {
+
+        public setHeight(height: number): Surface {
             this._height = height;
             this.svg.attr("height", height);
             return this;
         }
-        
-        setX(x: number): Surface {
+
+        public setX(x: number): Surface {
             this._x = x;
             this.svg.attr("x", x);
             return this;
         }
-        
-        setY(y: number): Surface {
+
+        public setY(y: number): Surface {
             this._y = y;
             this.svg.attr("y", y);
             return this;
         }
-        
-        getWidth(): number { return this._width; }
-        getHeight(): number { return this._height; }
-        
-        splitRows(n: number, verticalSpacingRatio: number): Array<Surface> {
+
+        public getWidth(): number { return this._width; }
+        public getHeight(): number { return this._height; }
+
+        public splitRows(n: number, verticalSpacingRatio: number): Array<Surface> {
             let rowHeight = n > 1 ? this._height * (1 - verticalSpacingRatio) / n : this._height;
             let marginHeight = n > 1 ? this._height * verticalSpacingRatio / (n - 1) : 0;
             return _.range(0, n).map((row) => {
@@ -57,8 +57,8 @@ module semio.core {
                     .setY(row * (rowHeight + marginHeight)); 
             });
         }
-        
-        splitColumns(n: number, horizontalSpacingRatio: number): Array<Surface> {
+
+        public splitColumns(n: number, horizontalSpacingRatio: number): Array<Surface> {
             let columnWidth = n > 1 ? this._width * (1 - horizontalSpacingRatio) / (n - 1) : this._width;
             let marginWidth = n > 1 ? this._width * horizontalSpacingRatio / (n - 1) : 0;
             return _.range(0, n).map((col) => {
@@ -70,20 +70,20 @@ module semio.core {
                     .setX(col * (columnWidth + marginWidth));
             });
         }
-        
-        splitGrid(n: number, maxColumns: number, verticalSpacingRatio: number, horizontalSpacingRatio: number): Array<Surface> {
+
+        public splitGrid(n: number, maxColumns: number, verticalSpacingRatio: number, horizontalSpacingRatio: number): Array<Surface> {
             let nColumns = Math.ceil(Math.sqrt(n));
             if (maxColumns) {
                 nColumns = Math.min(nColumns, maxColumns);
             }
             let nRows = Math.ceil(n / nColumns);
-            
+
             let cellWidth = this._width * (1 - horizontalSpacingRatio) / nColumns;
             let cellHeight = this._height * (1 - verticalSpacingRatio) / nRows;
-            
+
             let marginWidth = this._width * horizontalSpacingRatio / (nColumns - 1);
             let marginHeight = this._height * verticalSpacingRatio / (nRows - 1);
-            
+
             return _.flatMap(_.range(0, nRows), (row) => {
                 return _.range(0, nColumns)
                     .filter((col) => {
@@ -100,17 +100,17 @@ module semio.core {
                     });
             });
         }
-        
-        splitHeader(headerHeightRatio: number): SurfaceHeaderBody {
+
+        public splitHeader(headerHeightRatio: number): SurfaceHeaderBody {
             let headerHeight = Math.max(Math.min(headerHeightRatio * this._height, this._height), 0);
             let bodyHeight = this._height - headerHeight;
-            
+
             let headerId = this.containerId + "_header";
             let bodyId = this.containerId + "_body";
-            
+
             this.svg.append("g").attr("id", headerId);
             this.svg.append("g").attr("id", bodyId);
-            
+
             let headerSurface = new DrawingSurface(headerId)
                 .setWidth(this._width)
                 .setHeight(headerHeight)
@@ -119,50 +119,50 @@ module semio.core {
                 .setWidth(this._width)
                 .setHeight(bodyHeight)
                 .setY(headerHeight);
-                
+
             return {
                 header: headerSurface,
                 body: bodySurface
             };
         }
-        
-        addCenteredColumn(idSuffix: string, cx: number, width: number): Surface {
+
+        public addCenteredColumn(idSuffix: string, cx: number, width: number): Surface {
             let id = this.containerId + "_column_" + idSuffix;
-            
+
             this.svg.append("g").attr("id", id);
-            
+
             let surface = new DrawingSurface(id)
                 .setWidth(width)
                 .setHeight(this._height)
                 .setX(cx - width / 2);
-                
+
             return surface;
         }
-        
-        addCenteredRow(idSuffix: string, cy: number, height: number): Surface {
+
+        public addCenteredRow(idSuffix: string, cy: number, height: number): Surface {
             let id = this.containerId + "_row_" + idSuffix;
-            
+
             this.svg.append("g").attr("id", id);
-            
+
             let surface = new DrawingSurface(id)
                 .setWidth(this._width)
                 .setHeight(height)
                 .setY(cy - height / 2);
-                
+
             return surface;
         }
-        
-        addSurface(idSuffix: string, x: number, y: number, width: number, height: number): Surface {
+
+        public addSurface(idSuffix: string, x: number, y: number, width: number, height: number): Surface {
             let id = this.containerId + "_" + idSuffix;
-            
+
             this.svg.append("g").attr("id", id);
-            
+
             let surface = new DrawingSurface(id)
                 .setWidth(width)
                 .setHeight(height)
                 .setX(x)
                 .setY(y);
-            
+
             return surface;
         }
     }

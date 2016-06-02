cd29c8bc6656d7454d2c635ae680a4308b604c5d
Panos <codiply@gmail.com>
2016-06-02 06:59:44 +0100

    Linting fixes

--- src/core/ColorPalette.ts
+++ src/core/ColorPalette.ts
@@ -2,14 +2,14 @@
 
 module semio.core {
     export class ColorPalette {
-        private static _qualitativeColors12: Array<string> = 
-            ["#a6cee3","#1f78b4","#b2df8a","#33a02c","#fb9a99","#e31a1c",
-             "#fdbf6f","#ff7f00","#cab2d6","#6a3d9a","#ffff99","#b15928"];
+        private static _qualitativeColors12: Array<string> =
+            ["#a6cee3", "#1f78b4", "#b2df8a", "#33a02c", "#fb9a99", "#e31a1c",
+             "#fdbf6f", "#ff7f00", "#cab2d6", "#6a3d9a", "#ffff99", "#b15928"];
         private static _qualitativeColors20: Array<string> =
-            ["#1f77b4","#ff7f0e","#2ca02c","#d62728","#9467bd", 
-             "#8c564b","#e377c2","#7f7f7f","#bcbd22","#17becf",
-             "#aec7e8","#ffbb78","#98df8a","#ff9896","#c5b0d5",
-             "#c49c94","#f7b6d2","#c7c7c7","#dbdb8d","#9edae5"];
+            ["#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd",
+             "#8c564b", "#e377c2", "#7f7f7f", "#bcbd22", "#17becf",
+             "#aec7e8", "#ffbb78", "#98df8a", "#ff9896", "#c5b0d5",
+             "#c49c94", "#f7b6d2", "#c7c7c7", "#dbdb8d", "#9edae5"];
 
         public static qualitative(values: Array<string>): (x: string) => string  {
             let colors: Array<string>;

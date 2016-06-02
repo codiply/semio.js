caa7a41cf83ea41050468eb4d27f355f5c8a4c61
Panos <codiply@gmail.com>
2016-06-02 07:00:15 +0100

    Linting fixes

--- src/interfaces/Surface.ts
+++ src/interfaces/Surface.ts
@@ -9,23 +9,23 @@ module semio.interfaces {
     export interface Surface {
         containerId: string;
         svg: d3.Selection<any>;
-        
+
         setWidth(width: number): Surface;
         setHeight(width: number): Surface;
         setX(x: number): Surface;
         setY(y: number): Surface;
-        
+
         getWidth(): number;
         getHeight(): number;
-        
+
         splitRows(n: number, horizontalSpacingRatio: number): Array<Surface>;
         splitColumns(n: number, verticalSpacingRatio: number): Array<Surface>;
         splitGrid(n: number, maxColumns: number, verticalSpacingRatio: number, horizontalSpacingRatio: number): Array<Surface>;
         splitHeader(headerHeightRatio: number): SurfaceHeaderBody;
-        
+
         addCenteredColumn(idSuffix: string, cx: number, width: number): Surface;
         addCenteredRow(idSuffix: string, cy: number, height: number): Surface;
-        
+
         addSurface(idSuffix: string, x: number, y: number, width: number, height: number): Surface;
     }
 }

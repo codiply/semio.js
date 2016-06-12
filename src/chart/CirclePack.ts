85f69b6e3efd54047c247b8451d2cb42dd8089a0
Panos <codiply@gmail.com>
2016-06-12 17:57:14 +0100

    Add placeholder class for circle pack

---
+++ src/chart/CirclePack.ts
@@ -0,0 +1,27 @@
+/// <reference path="../../typings/d3/d3.d.ts"/>
+/// <reference path="../../typings/lodash/lodash.d.ts"/>
+/// <reference path="../interfaces/Context.ts"/>
+/// <reference path="../interfaces/Plotable.ts"/>
+/// <reference path="../interfaces/Surface.ts"/>
+
+namespace semio.chart {
+    import Context = semio.interfaces.Context;
+    import Plotable = semio.interfaces.Plotable;
+    import Surface = semio.interfaces.Surface;
+
+    export class CirclePack implements Plotable {
+        public getCategoricalColumns(): Array<string> {
+            return [];
+        }
+
+        public getNumericColumns(): Array<string> {
+            return [];
+        }
+
+        public plot(data: Array<any>, surface: Surface, context: Context): void {
+            if (!data) {
+                return;
+            }
+        }
+    }
+}

119514a90951bc8d0213b993ae080abec80a291a
Panos <codiply@gmail.com>
2016-06-30 20:16:25 +0100

    Add axes in Radar Chart

--- src/chart/RadarChart.ts
+++ src/chart/RadarChart.ts
@@ -11,9 +11,14 @@ namespace semio.chart {
 
     export class RadarChart implements Plotable {
         private _dimensionColumns: Array<string>;
+        private _dimensionAngle: { [col: string]: number };
 
         public dimensionColumns(columns: Array<string>): RadarChart {
             this._dimensionColumns = columns;
+            this._dimensionAngle = { };
+            columns.forEach((col, i) => {
+                this._dimensionAngle[col] = 2 *  Math.PI * i / this._dimensionColumns.length;
+            });
             return this;
         }
 
@@ -29,6 +34,29 @@ namespace semio.chart {
             if (!data) {
                 return;
             }
+
+            let width = surface.getWidth();
+            let height = surface.getHeight();
+            let radius = Math.min(width / 2, height / 2);
+            let centerX = width / 2;
+            let centerY = height / 2;
+
+            let svg = surface.svg;
+
+            let axes = svg.selectAll(".axis")
+                .data(this._dimensionColumns)
+                .enter()
+                .append("g")
+                .attr("class", "axis");
+
+            axes.append("line")
+                .attr("x1", centerX)
+                .attr("y1", centerY)
+                .attr("x2", (d) => centerX + radius * Math.sin(this._dimensionAngle[d]))
+                .attr("y2", (d) => centerY + radius * Math.cos(this._dimensionAngle[d]))
+                .attr("class", "line")
+                .style("stroke", "grey")
+                .style("stroke-width", "1px");
         }
     }
 }
